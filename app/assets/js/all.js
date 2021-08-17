9e81f1e2086d5446bb8dc00ea0c8ff61e0d376ec
onedog <honter_tribor@hotmail.com>
2021-08-18 00:08:27 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,22 @@
+  //  <!-- Initialize Swiper -->
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  spaceBetween: 25,
+  centeredSlides: true,
+  grabCursor: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  // loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+$(function() {
+  console.log('Hello Bootstrap5');
+});
+
