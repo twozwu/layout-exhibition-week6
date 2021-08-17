ee2b93ed981cfe7a9dedca96ec5fed59d62a0ee4
onedog <honter_tribor@hotmail.com>
2021-08-18 04:39:44 +0800

    20210818_navbar結構調整

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -2,14 +2,14 @@
 const swiper = new Swiper(".mySwiper", {
   slidesPerView: "auto",
   spaceBetween: 25,
-  centeredSlides: true,
+  centeredSlides: false,
   grabCursor: true,
   autoplay: {
     delay: 2500,
     disableOnInteraction: false,
   },
   loop: true,
-  // loopFillGroupWithBlank: true,
+  loopFillGroupWithBlank: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
